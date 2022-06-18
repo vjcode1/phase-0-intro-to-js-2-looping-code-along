d0c56ba3cd0d6a2967b61488612bd2ebdd84f49a
Vernitra Jones <vernitrasmedia@gmail.com>
2022-06-18 12:46:54 -0500

    commit tree 7ae0294a422c3766e240d5a791074d39dca8b276

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+const names = ["Charlie", "Samip", "Ali"];
+function writeCards(names, event) {
+  const thankYouCards = [];
+  for (let card = 0; card < names.length; card++) {
+    thankYouCards.push(`Thank you, ${names[card]}, for the wonderful ${event} gift!`);
+      }
+  return thankYouCards;
+}
+console.log(writeCards(names, "birthday"));
+function countDown(integer) {
+    while (integer >= 0) {
+    console.log(integer);
+    integer--;
+    }
+}
+countDown(5);
